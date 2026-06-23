946ad329a83e5755db72a6c2bc3e351303337e25
sotsokong799-ui <sotsokong799@gmail.com>
2026-06-23 15:49:38 +0700

    Update name control.js

--- name control.js
+++ name control.js
@@ -1,15 +1,31 @@
-function pumpOn() {
-  client.publish("esp32/pump", "ON");
+function pumpON()
+{
+  client.publish(
+    "esp32/pump",  // ◀️ កែពី "irrigation/control" មកជា "esp32/pump"
+    "ON"
+  );
 }
 
-function pumpOff() {
-  client.publish("esp32/pump", "OFF");
+function pumpOFF()
+{
+  client.publish(
+    "esp32/pump",  // ◀️ កែពី "irrigation/control" មកជា "esp32/pump"
+    "OFF"
+  );
 }
 
-function autoMode() {
-  client.publish("esp32/mode", "AUTO");
+function autoMode()
+{
+  client.publish(
+    "esp32/mode",  // ◀️ កែពី "irrigation/control" មកជា "esp32/mode"
+    "AUTO"
+  );
 }
 
-function manualMode() {
-  client.publish("esp32/mode", "MANUAL");
+function manualMode() // ◀️ បន្ថែមមុខងារនេះ ប្រសិនបើប៊ូតុង MANUAL លើ Web មិនទាន់ដើរ
+{
+  client.publish(
+    "esp32/mode",
+    "MANUAL"
+  );
 }
