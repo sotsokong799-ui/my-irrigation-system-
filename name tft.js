6b4005eea6848632b9baa00270796be750b2401a
sotsokong799-ui <sotsokong799@gmail.com>
2026-06-29 19:16:23 +0700

    Update name tft.js

--- name tft.js
+++ name tft.js
@@ -2,12 +2,9 @@ client.on('connect',()=>{
 
 console.log("MQTT Connected");
 
-client.subscribe("irrigation/soil");
-client.subscribe("irrigation/temp");
-client.subscribe("irrigation/humidity");
+client.subscribe("irrigation/Volt DC");
+client.subscribe("irrigation/Volt AC");
 client.subscribe("irrigation/tank");
 client.subscribe("irrigation/flow");
-client.subscribe("irrigation/temp");
-client.subscribe("irrigation/humidity");
 
 });
